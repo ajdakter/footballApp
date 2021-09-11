36ecdd5b773530e99c344749f43df05e292a546f
ajdakter <ajdaakter87@gmail.com>
2021-09-11 21:24:44 +0300

    Add dark theme

---
+++ app/src/main/java/com/ajdaakter/footballapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.ajdaakter.footballapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        delegate.applyDayNight()
+
+    }
+}
